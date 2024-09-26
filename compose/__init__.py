6e7dfb5a0102ce4dcd6118e06fee9aac4cec177a
Alexander Kozhevnikov <mentalisttraceur@gmail.com>
2024-09-26 17:05:42 +0000

    Modernization draft
    1. Bring typing file in-package (TODO: actually test).
    
    2. Make the automatically-async-if-needed behavior
       the default, renamed from `sacompose` to `compose`.
    
       * `acompose` remains the same: always awaitable,
         even if nothing in the composition is.
    
       * The no-async behavior is now renamed from
         `compose` to `scompose`.
    
    3. TODO: replace `setup.py` with something modern.
    
    4. TODO: update README.

--- compose/__init__.py
+++ compose/__init__.py
@@ -3,20 +3,36 @@
 
 """The classic ``compose``, with all the Pythonic features."""
 
-__all__ = ('compose', 'acompose', 'sacompose')
-__version__ = '1.6.2'
+__all__ = ('compose', 'acompose', 'scompose')
+__version__ = '2.0.0'
 
 
 from inspect import isawaitable as _isawaitable
+from inspect import iscoroutinefunction as _iscoroutinefunction
 from reprlib import recursive_repr as _recursive_repr
-
+try:
+    from inspect import markcoroutinefunction as _markcoroutinefunction
+except ImportError:
+    def _markcoroutinefunction(function):
+        pass
 
 def _name(obj):
     return type(obj).__name__
 
 
 class compose:
-    """Function composition: compose(f, g)(...) is equivalent to f(g(...))."""
+    """Function composition.
+
+    `compose(f, g)(...)` is equivalent to `f(g(...))`,
+    if neither `f` nor `g` return awaitables.
+
+    If either `f` or `g` return awaitables.
+    `await compose(f, g)(...)` is automatically either
+
+    * `await f(g(...))`,
+    * `f(await g(...))`, or
+    * `await f(await g(...))`.
+    """
 
     def __init__(self, *functions):
         """Initialize the composed function.
@@ -35,21 +51,31 @@ class compose:
         for function in reversed(functions):
             if not callable(function):
                 raise TypeError(_name(self) + '() arguments must be callable')
-            if isinstance(function, compose):
+            if (_iscoroutinefunction(function)
+            or  _iscoroutinefunction(type(function).__call__)):
+                _markcoroutinefunction(self)
+            if isinstance(function, (compose, acompose)):
                 _functions.extend(function.functions)
             else:
                 _functions.append(function)
         self.__wrapped__ = _functions[0]
         self._wrappers = tuple(_functions[1:])
 
-    def __call__(*args, **kwargs):
-        """Call the composed function."""
-        def __call__(self, *args):
-            return self, args
-        self, args = __call__(*args)
+    def __call__(self, /, *args, **kwargs):
+        """Call the composed function.
+
+        The return value will either be a normal value if all composed
+        callables return normal values, or an awaitable that needs an
+        `await` if at least one composed callable returns an awaitable.
+        """
         result = self.__wrapped__(*args, **kwargs)
-        for function in self._wrappers:
+        if _isawaitable(result):
+            return _finish(self._wrappers, result)
+        wrappers = iter(self._wrappers)
+        for function in wrappers:
             result = function(result)
+            if _isawaitable(result):
+                return _finish(wrappers, result)
         return result
 
     def __get__(self, obj, objtype=None):
@@ -78,17 +104,21 @@ class compose:
         return (self.__wrapped__,) + tuple(self._wrappers)
 
 
+async def _finish(remaining_functions, first_awaitable_result):
+    result = await first_awaitable_result
+    for function in remaining_functions:
+        result = function(result)
+        if _isawaitable(result):
+            result = await result
+    return result
+
+
 class acompose:
-    """Asynchronous function composition.
+    """Forced-asynchronous function composition.
 
-    await acompose(f, g)(...) is equivalent to one of
-
-        f(g(...))
-        await f(g(...))
-        f(await g(...))
-        await f(await g(...))
-
-    based on whether f and g return awaitable values.
+    `acompose` is like `compose`, except you always need
+    to do `await` to get the result, even if neither of
+    the functions being composed need `await`.
     """
 
     def __init__(self, *functions):
@@ -108,18 +138,16 @@ class acompose:
         for function in reversed(functions):
             if not callable(function):
                 raise TypeError(_name(self) + '() arguments must be callable')
-            if isinstance(function, (acompose, sacompose)):
+            if isinstance(function, (compose, acompose)):
                 _functions.extend(function.functions)
             else:
                 _functions.append(function)
         self.__wrapped__ = _functions[0]
         self._wrappers = tuple(_functions[1:])
+        _markcoroutinefunction(self)
 
-    async def __call__(*args, **kwargs):
+    async def __call__(self, /, *args, **kwargs):
         """Call the composed function."""
-        def __call__(self, *args):
-            return self, args
-        self, args = __call__(*args)
         result = self.__wrapped__(*args, **kwargs)
         if _isawaitable(result):
             result = await result
@@ -134,48 +162,48 @@ class acompose:
     functions = compose.functions
 
 
-class sacompose:
-    """Sometimes-asynchronous function composition.
+class scompose:
+    """Forced-synchronous function composition.
 
-    sacompose(f, g)(...) is equivalent to:
-
-        acompose(f, g)(...) if either f or g returns an awaitable object.
-        compose(f, g)(...) if neither f nor g returns an awaitable object.
+    `scompose(f, g)(...)` is equivalent to `f(g(...))`,
+    always without `await`, even if `f` or `g` return
+    awaitables.
     """
-    __init__ = acompose.__init__
 
-    def __call__(*args, **kwargs):
-        """Call the composed function.
+    def __init__(self, *functions):
+        """Initialize the composed function.
 
-        The return value will either be a normal value if all composed
-        callables return normal values, or an awaitable that needs an
-        `await` if at least one composed callable returns an awaitable.
+        Arguments:
+            *functions: Functions (or other callables) to compose.
+
+        Raises:
+            TypeError:
+                If no arguments are given.
+                If any argument is not callable.
         """
-        def __call__(self, *args):
-            return self, args
-        self, args = __call__(*args)
+        if not functions:
+            raise TypeError(_name(self) + '() needs at least one argument')
+        _functions = []
+        for function in reversed(functions):
+            if not callable(function):
+                raise TypeError(_name(self) + '() arguments must be callable')
+            if isinstance(function, scompose):
+                _functions.extend(function.functions)
+            else:
+                _functions.append(function)
+        self.__wrapped__ = _functions[0]
+        self._wrappers = tuple(_functions[1:])
+
+    def __call__(self, /, *args, **kwargs):
+        """Call the composed function."""
         result = self.__wrapped__(*args, **kwargs)
-        if _isawaitable(result):
-            return _finish(self._wrappers, result)
-        wrappers = iter(self._wrappers)
-        for function in wrappers:
+        for function in self._wrappers:
             result = function(result)
-            if _isawaitable(result):
-                return _finish(wrappers, result)
         return result
 
     __repr__ = compose.__repr__
     __get__ = compose.__get__
     functions = compose.functions
-
-
-async def _finish(remaining_functions, first_awaitable_result):
-    result = await first_awaitable_result
-    for function in remaining_functions:
-        result = function(result)
-        if _isawaitable(result):
-            result = await result
-    return result
 
 
 # Portability to some minimal Python implementations:
@@ -184,4 +212,4 @@ try:
 except AttributeError:
     compose.__name__ = 'compose'
     acompose.__name__ = 'acompose'
-    sacompose.__name__ = 'sacompose'
+    scompose.__name__ = 'scompose'
